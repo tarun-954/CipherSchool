f3fca876c0a1f25d4628bb2d520627b983c791ee
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-07-04 17:10:02 +0530

    Lecture 6

---
+++ Week 1/lecture 6.js
@@ -0,0 +1,22 @@
+
+// local variable and global and const keyword
+// var x=15;
+// {
+//     let x=5;
+//     console.log(x);
+// }
+// console.log(x);
+// const y=10;
+// console.log(y);
+// y=5;
+// console.log(y);
+
+//  Writing a function in js 
+// normal function
+var x= function (x,y){
+    return x+y;
+} 
+
+// arrow function 
+const a=(x,y)=>x+y;
+console.log(a(10,10));
