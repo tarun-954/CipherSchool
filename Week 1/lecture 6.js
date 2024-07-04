055d0176207a37afe8cddc4d50a951edc4df83c8
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-07-04 18:03:16 +0530

    lecture 6

--- Week 1/lecture 6.js
+++ Week 1/lecture 6.js
@@ -20,3 +20,51 @@ var x= function (x,y){
 // arrow function 
 const a=(x,y)=>x+y;
 console.log(a(10,10));
+
+// spread operator
+const q1=["jan","febuery","march"];
+const q2=["April","May","June"];
+const q3=["july","August","September"];
+const q4=["octuber","november","December"];
+const month=[...q1,...q2,...q3,...q4];
+console.log(month);
+
+
+//inbuild function iwth spread operator
+const mynumbers=[25,46,32,84,64,43];
+let maxValue=Math.max(...mynumbers);
+console.log(maxValue);
+
+//for of loop 
+let sum=0;
+for(let num of mynumbers){
+    sum=sum+num;
+}
+console.log(sum);
+
+//for of to count print the character of the string
+const name="TarunChoudhary";
+let text="";
+for(let ch of name){
+    text=text+ch+" ";
+
+}
+console.log(text);
+// very important 
+//hash in js 
+
+const fruits=new Map([
+    ["apples",500],
+    ["grapes",300],
+    ["oranges",800]
+]);
+console.log(fruits);
+console.log(fruits.get("oranges"));
+
+
+// sets in js
+const letter =new Set();
+letter.add("a");
+letter.add("b");
+letter.add('a');
+console.log(letter);
